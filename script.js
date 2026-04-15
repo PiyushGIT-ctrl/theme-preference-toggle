5b9fcd240134391931dd69b8682b29d62a565afe
Piyush <sharmapiyushkv@gmail.com>
2026-04-15 20:22:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+function setDarkorLight () {
+    if(window.matchMedia("(prefers-color-scheme: dark)").matches){
+        document.body.classList.add("dark");
+        document.body.classList.remove("light");
+    } else{
+        document.body.classList.add("light");
+        document.body.classList.remove("dark");
+    }
+};
+
+setDarkorLight();
+
+if(localStorage.getItem("theme")){
+    document.body.classList.add(localStorage.getItem("theme"));
+}
+
+let button = document.querySelector("button");
+button.addEventListener("click", function(){
+    if(document.body.classList.contains("dark")){
+        document.body.classList.remove("dark");
+        document.body.classList.add("light");
+
+        localStorage.setItem("theme", "light");
+    } else {
+        document.body.classList.remove("light");
+        document.body.classList.add("dark");
+
+        localStorage.setItem("theme", "dark");
+    }
+});
+
+window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", function(){
+    setDarkorLight();
+});
